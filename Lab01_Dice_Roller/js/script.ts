6797ce904ea190fd2dc30efbe2193ae0409316b6
Mark Okulov <mvokulov@gmail.com>
2017-11-24 21:47:03 +0300

    add rollDie function mock

--- Lab01_Dice_Roller/js/script.ts
+++ Lab01_Dice_Roller/js/script.ts
@@ -2,23 +2,26 @@ let text : string = "one";
 let squareSizeNumber : number = 100;
 let squareSize : string = `${ squareSizeNumber }px`;
 let div : Element = document.createElement('div');
+let button : Element = document.createElement('button');
+button.textContent = "Roll the Dice";
 
 (div as HTMLElement).style.width = squareSize;
 (div as HTMLElement).style.height = squareSize;
 (div as HTMLElement).style.border = "4px solid black";
-(div as HTMLElement).textContent = text;
+
 (div as HTMLElement).style.textAlign = "center";
 (div as HTMLElement).style.fontSize = "2em";
 (div as HTMLElement).style.display = "table-cell";
 (div as HTMLElement).style.verticalAlign = "middle";
 
 document.body.appendChild(div);
+document.body.appendChild(button);
 
-enum RollResult {
-    One = 1,
-    Two,
-    Three,
-    Four,
-    Five,
-    Six
-} 
+let rollDie : Function = (elem : Element, result : string) : boolean => {
+    (div as HTMLElement).textContent = result;
+    return true;
+}
+
+(button as HTMLElement).onclick = (event) => {
+    rollDie(div, text);
+}
